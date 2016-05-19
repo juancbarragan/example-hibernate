05af940535e64a4da2e34d645de526e87f08a06a
Juan Carlos Barragan Quintero <juancarlosbarraganquintero@Juans-Air.dev.au.utiba.com>
2016-05-19 10:59:46 +1000

    Added find all contacts from all books, added more javadoc and introduced ContactNotFoundException

---
+++ src/main/java/com/reece/exception/ContactNotFoundException.java
@@ -0,0 +1,15 @@
+package com.reece.exception;
+
+/**
+ * Custom Exception for Object not found.
+ * 
+ * @author juancarlosbarraganquintero
+ */
+public class ContactNotFoundException extends Exception {
+
+    public ContactNotFoundException(String message) {
+        super(message);
+    }
+    
+    
+}
